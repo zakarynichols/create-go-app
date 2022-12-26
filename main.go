1c90948bb69448068aa532fd04c7a972e82acc2c
Zakary Nichols <zakarynichols@gmail.com>
2022-12-26 04:55:38 -0500

    use better time api; shorter var for colors; update terminal output

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func main() {
 
 	start := time.Now()
 
-	color := colors.New()
+	col := colors.New()
 
 	pkg := new(pkg)
 
@@ -29,91 +29,91 @@ func main() {
 	flags := flag.Args()
 
 	if len(flags) != 1 {
-		fmt.Printf("%serror: only one non-named flag argument allowed.%s\n", colors.Red, colors.Reset)
+		fmt.Printf("%serror: only one non-named flag argument allowed.%s\n", col.Red, col.Reset)
 		os.Exit(1)
 	}
 
 	pkg.name = flags[0] // Will make this 'smarter' with help message and such.
 
+	dir, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("%sCreating a new Go app %s%s%s %s\n", color.Cyan, color.Green, pkg.name, color.Cyan, color.Reset)
+	fmt.Printf("%sCreating a new %sGo%s app in %s%s/%s%s %s\n", col.White, col.Cyan, col.White, col.Green, dir, pkg.name, col.Cyan, col.Reset)
 
 	fmt.Print("\n")
 
-	fmt.Printf("%sChecking if %s./%s%s already exists...%s\n", color.Cyan, color.Green, pkg.name, color.Cyan, color.Reset)
+	fmt.Printf("%sChecking if %s./%s%s already exists...%s\n", col.White, col.Green, pkg.name, col.White, col.Reset)
 	_, err = os.Open("./" + pkg.name)
 	if err == nil {
 		fmt.Printf("\n")
-		fmt.Printf("%serror: directory "+"'./"+"%s"+"'"+" already exists%s\n", color.Red, pkg.name, color.Reset)
+		fmt.Printf("%serror: directory "+"'./"+"%s"+"'"+" already exists%s\n", col.Red, pkg.name, col.Reset)
 		fmt.Print("\n")
 		os.Exit(1)
 	}
 
 	fmt.Print("\n")
 
-	fmt.Printf("%sMaking new dir %s./%s%s\n", color.Cyan, color.Green, pkg.name, color.Reset)
+	fmt.Printf("%sMaking new dir %s./%s%s\n", col.White, col.Green, pkg.name, col.Reset)
 	err = os.Mkdir(pkg.name, 0750)
 	if err != nil {
 		fmt.Printf("\n")
-		fmt.Printf("%serror: failed to create directory\n%s", colors.Red, colors.Reset)
+		fmt.Printf("%serror: failed to create directory\n%s", col.Red, col.Reset)
 		fmt.Print("\n")
 		os.Exit(1)
 	}
 
 	fmt.Print("\n")
 
-	fmt.Printf("%sWriting %smain.go%s file...%s\n", color.Cyan, color.Green, color.Cyan, color.Reset)
+	fmt.Printf("%sWriting %smain.go%s file...%s\n", col.White, col.Cyan, col.White, col.Reset)
 	err = os.WriteFile(pkg.name+"/main.go", []byte(mainTemplate), 0660)
 	fmt.Print("\n")
 	if err != nil {
 		fmt.Printf("\n")
-		fmt.Printf("%serror: failed to write files\n%s", colors.Red, colors.Reset)
+		fmt.Printf("%serror: failed to write files\n%s", col.Red, col.Reset)
 		fmt.Print("\n")
 		os.Exit(1)
 	}
 
-	fmt.Printf("%sChanging to dir ./%s%s\n", color.Cyan, pkg.name, color.Reset)
+	fmt.Printf("%sChanging to dir: %scd %s./%s%s\n", col.White, col.Cyan, col.Green, pkg.name, col.Reset)
 	err = os.Chdir("./" + pkg.name)
 	if err != nil {
 		fmt.Printf("\n")
-		fmt.Printf("%serror: failed to change directory\n%s", colors.Red, colors.Reset)
+		fmt.Printf("%serror: failed to change directory\n%s", col.Red, col.Reset)
 		fmt.Print("\n")
 		os.Exit(1)
 	}
 
 	fmt.Print("\n")
 
-	fmt.Printf("%sExecuting go mod init...%s\n", color.Cyan, color.Reset)
-	cmd := exec.Command("go", "mod", "init", "example/my-module") // don't hardcode module name. only for testing
+	fmt.Printf("%sInitializing a module: %sgo mod init %s%s\n", col.White, col.Cyan, pkg.name, col.Reset)
+	cmd := exec.Command("go", "mod", "init", pkg.name) // don't hardcode module name. only for testing
 	err = cmd.Run()
 	if err != nil {
 		fmt.Printf("\n")
-		fmt.Printf("%serror: failed to initialize a module\n%s", colors.Red, colors.Reset)
+		fmt.Printf("%serror: failed to initialize a module\n%s", col.Red, col.Reset)
 		fmt.Print("\n")
 		os.Exit(1)
 	}
 
 	fmt.Print("\n")
 
-	fmt.Printf("%sExecuting go fmt...%s\n", color.Cyan, color.Reset)
+	fmt.Printf("%sFormatting code: %sgo fmt ./...%s\n", col.White, col.Cyan, col.Reset)
 	cmd = exec.Command("go", "fmt", "./...")
 	err = cmd.Run()
 	if err != nil {
 		fmt.Printf("\n")
-		fmt.Printf("%serror: failed to format code\n%s", colors.Red, colors.Reset)
+		fmt.Printf("%serror: failed to format code\n%s", col.Red, col.Reset)
 		fmt.Print("\n")
 		os.Exit(1)
 	}
 
-	t := time.Now()
-	elapsed := t.Sub(start)
+	elapsed := time.Since(start)
 
 	fmt.Print("\n")
 
-	fmt.Printf("%sSucceeded in %f seconds\n", colors.Green, elapsed.Seconds())
+	fmt.Printf("%sSucceeded in %f seconds\n", col.Green, elapsed.Seconds())
 }
 
 var mainTemplate = `
